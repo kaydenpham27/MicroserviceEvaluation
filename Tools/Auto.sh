4d828f67739bc6b2fb4ca576d80b3334a56bc870
Kien Pham <124032637+kaydenpham27@users.noreply.github.com>
2024-08-04 05:04:44 +1000

    Update Auto.sh

--- Tools/Auto.sh
+++ Tools/Auto.sh
@@ -3,14 +3,16 @@
 # Directory to monitor
 DIRECTORY="$PWD"
 HASHSTRING_STORAGE="$PWD/DigestString.txt"
+HASHUNIQUE_STORAGE="$PWD/DigestUnique.txt"
 HASH_STORAGE="$PWD/Digest.txt"
+BASELINE="$PWD/Baseline.txt"
 PROBABILITY_STORAGE="$PWD/Distribution.txt"
 INCONSISTENCY_STORAGE="$PWD/Inconsistency.txt"
 TOOL="$PWD/Tool.py"
 
 # Function to calculate the hash of a file
 calculate_hash() {
-    python3 "$TOOL" "$1" "$2" "$3" "$4" "$5"
+    python3 "$TOOL" "$1" "$2" "$3" "$4" "$5" "$6"
 }
 
 # Function to calculate the current probability distribution
@@ -18,12 +20,38 @@ calculate_probability() {
     python3 "$TOOL" "$1" "$2" "$3" "$4"
 }
 
+# Function to detect inconsistencies between newly created trace and baseline trace
+detecting_abnormal_behavior() {
+    python3 "$TOOL" "$1" "$2" "$3" "$4" "$5"
+}
+
+# Ensure necessary files exist
+create_if_not_exists() {
+    if [ ! -f "$1" ]; then
+        touch "$1"
+        echo "Created file: $1"
+    fi
+}
+
+create_if_not_exists "$HASHSTRING_STORAGE"
+create_if_not_exists "$HASHUNIQUE_STORAGE"
+create_if_not_exists "$HASH_STORAGE"
+create_if_not_exists "$BASELINE"
+create_if_not_exists "$PROBABILITY_STORAGE"
+create_if_not_exists "$INCONSISTENCY_STORAGE"
+
 # Monitor the directory for new files
 inotifywait -m -e create --format '%w%f' "$DIRECTORY" | while read FILE
 do
     echo "New file detected: $FILE"
     sleep 5
-    HASH=$(calculate_hash "CreatingDigest" "$FILE" "$HASHSTRING_STORAGE" "$HASH_STORAGE" 2)
+    # Create Digest for newly created trace
+    HASH=$(calculate_hash "CreateDigest" "$FILE" "$HASHSTRING_STORAGE" "$HASH_STORAGE" "$HASHUNIQUE_STORAGE" 2)
     sleep 5
-    PROBABILITY=$(calculate_probability "CalculatingProbability" "$HASH_STORAGE" "$PROBABILITY_STORAGE" "$INCONSISTENCY_STORAGE")
+    # Detects inconsistencies between newly created trace and baseline trace
+    INCONSISTENCY=$(detecting_abnormal_behavior "DetectInconsistency" "$FILE" "$BASELINE" "$INCONSISTENCY_STORAGE" 2)
+    sleep 5
+    # Updates the frequency distribution
+    PROBABILITY=$(calculate_probability "UpdateFrequency" "$BASELINE" "$HASH_STORAGE" "$PROBABILITY_STORAGE")
+    sleep 5
 done
